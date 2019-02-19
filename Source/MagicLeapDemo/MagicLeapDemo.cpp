b16eae243af3aebec58f1fe43b5250e0b5832f54
Ryan Gadz <ryangadz@msn.com>
2019-02-18 20:20:08 -0800

    Initial Project created

---
+++ Source/MagicLeapDemo/MagicLeapDemo.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MagicLeapDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MagicLeapDemo, "MagicLeapDemo" );
